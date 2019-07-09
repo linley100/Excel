a200127c751ff9fe49bc1789a5243e3fc3e20f8a
linley100 <51034403+linley100@users.noreply.github.com>
2019-07-09 00:42:30 -0400

    1

--- src/xls.ts
+++ src/xls.ts
@@ -4,11 +4,20 @@ export function imprimir() {
     /* create new workbook */
     let workbook = XLSX.utils.book_new();
     let i = 5
-    let ws_data = [['hello' , 'world']];
+    let ws_data = [['','hello' , 'world'],['','hello' , 'world']];
     let ws = XLSX.utils.aoa_to_sheet(ws_data);
+
+	let address = 'B3';
+	ws[address] = 'loco';
+
+	ws['C5'] = { t:'s', v: "cosas"};
+	ws['C1'] = { t:'n', v: 5 };
+	ws['A3'] = { t:'l', Target:"http://sheetjs.com", Tooltip:"Find us @ SheetJS.com!" };
+	ws['A5'] = ({ t:'n', W: 'muchas cosas'});
+	ws['A6'] = ({ t:'z', v: 'Ncosas'});
 
     workbook.SheetNames.push("Test Sheet");
     workbook.Sheets["Test Sheet"] = ws;
 
-    XLSX.writeFile(workbook, 'out.xls');
+    XLSX.writeFile(workbook, 'prueba.xls');
 };
